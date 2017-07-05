1b48ae83416e26b68de5ba3355d99ddd47b61956
Joakim Andersson <nils-joakim.andersson@hotmail.com>
2017-07-05 09:40:28 +0200

    Change a few settings

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'eid%)g(431v0*c4qa^15g@4@2+r0t=6jf4(q!v3sd7bsgz@_17'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '*']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Stockholm'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
